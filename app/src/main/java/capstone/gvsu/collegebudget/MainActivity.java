aaf9b21ef64136d42f1a22f6ad6cf34fe1cb8a61
olesaka <olesaka@mail.gvsu.edu>
2018-09-19 14:18:27 -0400

    Began google sign in process

--- app/src/main/java/capstone/gvsu/collegebudget/MainActivity.java
+++ app/src/main/java/capstone/gvsu/collegebudget/MainActivity.java
@@ -1,13 +1,41 @@
 package capstone.gvsu.collegebudget;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.firebase.auth.FirebaseAuth;
+
 public class MainActivity extends AppCompatActivity {
+
+    SignInButton button;
+    FirebaseAuth auth;
+    private final static int RC_SIGN_IN = 2;
+    GoogleSignInClient mGoogleSignInClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        button = (SignInButton) findViewById((R.id.googleButton));
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
     }
+
+    // Configure Google Sign In
+    GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id))
+            .requestEmail()
+            .build();
+
+    private void signIn() {
+        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
+
 }
