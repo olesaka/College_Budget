e48e4d83da38ac6150c814217a403a5a8673d9de
Jake Gonser <gonserj@mail.gvsu.edu>
2018-10-30 20:26:19 -0400

    added transaction history to Category.java with appropriate functions

--- app/src/main/java/capstone/gvsu/collegebudget/Category.java
+++ app/src/main/java/capstone/gvsu/collegebudget/Category.java
@@ -2,18 +2,21 @@ package capstone.gvsu.collegebudget;
 
 
 import android.widget.Button;
+import java.util.ArrayList;
 
 public class Category {
     private double spent;
     private double budgeted;
     private String name;
     private Button editButton;
+    private ArrayList transactionHistory;
 
     public Category(String name, double spent, double budgeted, Button editButton) {
         this.spent = spent;
         this.budgeted = budgeted;
         this.name = name;
         this.editButton = editButton;
+        this.transactionHistory = new ArrayList();
     }
     public void setName(String name) {
         this.name = name;
@@ -21,10 +24,13 @@ public class Category {
     public void setSpent(double spent) {
         this.spent = spent;
     }
-
     public void setBudgeted(double budgeted) {
         this.budgeted = budgeted;
     }
+    public void setTransactionHistory(ArrayList th){
+        this.transactionHistory = th;
+    }
+
     public double getSpent() {
         return this.spent;
     }
@@ -33,5 +39,8 @@ public class Category {
     }
     public String getName() {
         return name;
+    }
+    public ArrayList getTransactionHistory(){
+        return this.transactionHistory;
     }
 }
