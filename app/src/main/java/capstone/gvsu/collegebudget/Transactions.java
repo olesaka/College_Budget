76258a1f1b955ae0bfe60ff97a2bbdd39a334844
olesaka <olesaka@mail.gvsu.edu>
2018-10-30 21:04:15 -0400

    started process of showing transactions on category activity

--- app/src/main/java/capstone/gvsu/collegebudget/Transactions.java
+++ app/src/main/java/capstone/gvsu/collegebudget/Transactions.java
@@ -1,12 +1,15 @@
 package capstone.gvsu.collegebudget;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Parcelable;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.google.firebase.database.DataSnapshot;
@@ -21,6 +24,7 @@ public class Transactions extends AppCompatActivity implements View.OnClickListe
     private Database database;
     private Button deleteButton;
     private Button backButton;
+    private LinearLayout linLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,6 +41,7 @@ public class Transactions extends AppCompatActivity implements View.OnClickListe
         deleteButton.setOnClickListener(this);
         backButton = findViewById(R.id.back);
         backButton.setOnClickListener(this);
+        linLayout = findViewById(R.id.transLayout);
     }
 
     public void deleteCategory(){
@@ -68,7 +73,11 @@ public class Transactions extends AppCompatActivity implements View.OnClickListe
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
                 for (DataSnapshot child : dataSnapshot.getChildren()) {
-
+                    LayoutInflater inflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+                    final View rowView = inflater.inflate(R.layout.transaction_line, null);
+                    linLayout.addView(rowView, linLayout.getChildCount() - 1);
+                    String date = child.getKey();
+                    String amount = "$" + child.getValue();
                 }
             }
 
